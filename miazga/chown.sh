4e32762ab195432f97443f9327f7d044e5858659
knight <knight@oedipus>
2013-05-16 00:28:04 +0200

    chmod.sh improve and add examples

--- miazga/chown.sh
+++ miazga/chown.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env sh
 
-#Name:		chmod.sh
+#Name:		chown.sh
 #Author: 	knajt, zic.spse(AT)gmail.com
 #Date: 		2013
-#Version: 	2013.0
+#Version: 	2013.1
 
 #export LC_ALL = your_enviroment
 
-VERSION="2013.0"
+VERSION="2013.1"
 
 `true`
 EXIT_SUCCESS=$?
 
 SCRIPT_NAME=`basename $0`
 ARGC=$#
-
-FIND_PATH="/home"
-
 
 ##
 #
@@ -30,16 +27,18 @@ function print_help {
 		${SCRIPT_NAME} script for changes owner/group folder
 	options:
 
-		[v] - print version of program
-		[h] - print this message
-		[f] - path to file with [users,group,path](csv format)
-		
+		[-v] - print version of program
+		[-h] - print this message
+		[-f] - path to file with [users,group,path](csv format)
+		[-b] - base path with parameter -f [user,group](csv format)		
 	examples:
 
 		${SCRIPT_NAME}	
 			#${SCRIPT_NAME} implicit works with /home directories.
 
-		${SCRIPT_NAME} -f file.txt"
+		${SCRIPT_NAME} -f file.txt
+		${SCRIPT_NAME} -f file.txt -b /users
+"
 }
 
 function enum_error {
@@ -72,8 +71,6 @@ function enum_string {
                 E_FILE) echo >&2 "Something is wrong with file.";;
         esac
 }
-
-
 
 function check_params {
 
@@ -108,28 +105,74 @@ function check_params {
         
 	        enum_string E_VER; exit $(enum_error E_OK)
         fi;
+	
+	if [ $file_flag = "on" ] ; then
+
+		if [ ! -f "$file_path" ] && [ ! -r "$file_path" ] ; then
+
+			enum_string E_FILE; exit $(enum_error E_FILE)
+
+		fi;
+
+	fi;
+	
+	if [ $path_flag = "on" ] && [ $file_flag = "off" ] ; then
+
+		enum_string E_BPAR; exit $(enum_error E_FILE)
+	fi;
+
 }
 
-
-
-function change_owner {
+function default_change_owner {
 
 	for temp in $1 
 	do
 		user=`basename "$temp"`
 	  	group=$user	
 	
-		chown -R "${user}:${group}" "${FIND_PATH}/${user}"
+		chown -R "${user}:${group}" "${path_value}/${user}"
 	done
 }
 
 function find_owner_group {
 
 	maxdepth=1
-	value=`find ${FIND_PATH} -maxdepth ${maxdepth}` 
-	without_find_path=` echo $value | sed s#${FIND_PATH}##`	
+	value=`find ${path_value} -maxdepth ${maxdepth}` 
+	without_find_path=` echo $value | sed s#${path_value}##`	
 
-	change_owner "$without_find_path"
+	default_change_owner "$without_find_path"
+}
+
+function pathCSV_notspec_read_file {
+	
+	OLD_IFS=$IFS
+	IFS=,
+
+	while read user_csv group_csv 
+	do
+		chown -R "${user_csv}:${group_csv}" "${path_value}"
+
+	done < "$file_path" 
+
+
+	IFS=$OLD_IFS
+
+}
+
+function pathCSV_spec_read_file {
+	
+	OLD_IFS=$IFS
+	IFS=,
+
+	while read user_csv group_csv path_csv 
+	do
+		chown -R "${user_csv}:${group_csv}" "${path_csv}"
+
+	done < "$file_path" 
+
+
+	IFS=$OLD_IFS
+
 }
 
 ##
@@ -148,8 +191,11 @@ help_flag="off"
 #vERSION flag
 ver_flag="off"
 
+#pATH flag
+path_flag="off"
+path_value="/home"
 
-while getopts vhf:  opt
+while getopts vhf:p:  opt
 do
     case "$opt" in
         h)      if [ $help_flag = "on" ] ; then
@@ -169,6 +215,12 @@ do
                 fi;
                 ver_flag="on"
                 ;;
+        p)      if [ $path_flag = "on" ] ; then
+                        enum_string E_BPAR ; exit $(enum_error E_BPAR)
+                fi;
+                path_flag="on"; path_value="$OPTARG"
+                ;;
+
 
         \?|*)
                  enum_string E_BPAR ; exit $(enum_error E_BPAR);;
@@ -181,8 +233,20 @@ shift $( expr $OPTIND - 1 )
 #	Main program
 #
 ##
+
 check_params
-find_owner_group
+
+if [ $file_flag = "off" ] && [ $path_flag = "off"  ]  ; then
+
+	find_owner_group
+elif [ $file_flag = "on" ] ; then
+
+	pathCSV_notspec_read_file	
+elif [ $path_flag = "on" ] ; then
+
+	pathCSV_spec_read_file
+fi;
+
 
 exit $(enum_error E_OK)
 
